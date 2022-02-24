f1e131ad6934aafa30958ec27537058830f5aca7
l1ttps <l1ttps443@gmail.com>
2022-02-24 09:54:07 +0700

    feat: connect database postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env"
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [
+        __dirname + '/**/*.entity.{js,ts}'
+      ],
+      keepConnectionAlive: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
